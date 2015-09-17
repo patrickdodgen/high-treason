75c0c25c0fd09eba77c25b99a2cae2a94c25ccab
unknown <wmatson@clearwateranalytics.com>
2015-09-17 11:11:46 -0600

    Formatting

--- high-treason.js
+++ high-treason.js
@@ -6,46 +6,46 @@ if (Meteor.isClient) {
   Meteor.subscribe("games");
   Meteor.subscribe("roles");
   var Router = Backbone.Router.extend({
-	  routes: {
-		"games/create" : "lobbyCreation",
-		"lobby" : "lobby",
-		"games": "games",
-		"board" : "board",
-		"": "splash"
-	  },
-	  splash: function () {
-	   Session.set('currentPage', 'splash');
-	  },
-	  games: function () {
-	   Session.set('currentPage', 'games');
-	  },
-	  lobbyCreation: function () {
-	   Session.set('currentPage', 'lobbyCreation');
-	  },
-	  lobby: function () {
-	   Session.set('currentPage', 'lobby');
-	  },
-	  board: function() {
-	   Session.set('currentPage', 'board');
-	  }
+    routes: {
+      "games/create": "lobbyCreation",
+      "lobby": "lobby",
+      "games": "games",
+      "board": "board",
+      "": "splash"
+    },
+    splash: function() {
+      Session.set('currentPage', 'splash');
+    },
+    games: function() {
+      Session.set('currentPage', 'games');
+    },
+    lobbyCreation: function() {
+      Session.set('currentPage', 'lobbyCreation');
+    },
+    lobby: function() {
+      Session.set('currentPage', 'lobby');
+    },
+    board: function() {
+      Session.set('currentPage', 'board');
+    }
   });
 
   var app_router = new Router;
 
-  Backbone.history.start({ pushState : true});
+  Backbone.history.start({
+    pushState: true
+  });
 
   if (!Meteor.user()) {
-	  Session.set('currentPage', 'splash');
-  }
-  else
-  {
-	  checkGameState();
+    Session.set('currentPage', 'splash');
+  } else {
+    checkGameState();
   }
 
   Template.body.events({
-	"click .logout": function () {
+    "click .logout": function() {
       Meteor.logout();
-	  Session.set('currentPage', 'splash');
+      Session.set('currentPage', 'splash');
     },
   });
 
@@ -53,44 +53,44 @@ if (Meteor.isClient) {
     passwordSignupFields: "USERNAME_ONLY"
   });
 
-  Accounts.onLogin( function (){
-	  Session.set('currentPage', 'games');
+  Accounts.onLogin(function() {
+    Session.set('currentPage', 'games');
   });
 
   checkGameState = function() {
-      var gameId = Session.get("currentGame");
-      if (!gameId)
-      {
-        var game = Games.findOne({ players: Meteor.user().username});
-        if (game)
-          gameId = game._id;
-      }
-      if (!gameId) {
-        Session.set('currentPage', 'games');
-      }
-      else {
-        Session.set('currentPage', 'lobby');
-        Session.set('currentGame', gameId);
-      }
+    var gameId = Session.get("currentGame");
+    if (!gameId) {
+      var game = Games.findOne({
+        players: Meteor.user().username
+      });
+      if (game)
+        gameId = game._id;
     }
+    if (!gameId) {
+      Session.set('currentPage', 'games');
+    } else {
+      Session.set('currentPage', 'lobby');
+      Session.set('currentGame', gameId);
+    }
+  }
 }
 
 if (Meteor.isServer) {
 
-  Accounts.validateNewUser(function(user){
-      if(user.username && /.*\(Bot\)/.test(user.username))
-        throw new Meteor.Error(403, "That user already exists.");
-      if(user.username && user.username.length >= 3)
-        return true;
-      throw new Meteor.Error(403, "Usernames must be at least 3 characters long.");
+  Accounts.validateNewUser(function(user) {
+    if (user.username && /.*\(Bot\)/.test(user.username))
+      throw new Meteor.Error(403, "That user already exists.");
+    if (user.username && user.username.length >= 3)
+      return true;
+    throw new Meteor.Error(403, "Usernames must be at least 3 characters long.");
   });
 
   // This code only runs on the server
-  Meteor.publish("games", function () {
+  Meteor.publish("games", function() {
     return Games.find();
   });
 
-  Meteor.publish("roles", function () {
+  Meteor.publish("roles", function() {
     return Roles.find();
   });
 }
--- server/game.js
+++ server/game.js
@@ -1,9 +1,8 @@
 var auth = function(fn) {
-  return function(){
-    if(!Meteor.userId()) {
+  return function() {
+    if (!Meteor.userId()) {
       throw new Meteor.Error('not-authorized');
-    }
-    else {
+    } else {
       fn.apply(this, arguments);
     }
   };
@@ -11,57 +10,65 @@ var auth = function(fn) {
 
 var game = function(fn) {
   return auth(function(gameId) {
-    if(!gameId)
-      throw new Meteor.Error("invalid game-id: "+gameId);
-    var game = Games.findOne({_id:gameId});
-    if(!game)
-      throw new Meteor.Error("invalid game-id: "+gameId);
+    if (!gameId)
+      throw new Meteor.Error("invalid game-id: " + gameId);
+    var game = Games.findOne({
+      _id: gameId
+    });
+    if (!game)
+      throw new Meteor.Error("invalid game-id: " + gameId);
     arguments[0] = game;
     fn.apply(this, arguments);
   });
 };
 
 Meteor.methods({
-  createGame:auth(function(maxPlayers, chosenRoles) {
-		if (maxPlayers < CONSTANTS.minPlayers || maxPlayers > CONSTANTS.maxPlayers)
-			throw new Meteor.Error("invalid number of players specified");
-		Games.insert({
-			players: [Meteor.user().username],
-			currentPlayerCount: 1,
-			missionLayout: CONSTANTS.missionLayouts[maxPlayers-5],
-			maxPlayers: maxPlayers,
-			chosenRoles: chosenRoles,
-			createdAt: new Date(),
-			owner: Meteor.userId(),
-			username: Meteor.user().username
-		});
+  createGame: auth(function(maxPlayers, chosenRoles) {
+    if (maxPlayers < CONSTANTS.minPlayers || maxPlayers > CONSTANTS.maxPlayers)
+      throw new Meteor.Error("invalid number of players specified");
+    Games.insert({
+      players: [Meteor.user().username],
+      currentPlayerCount: 1,
+      missionLayout: CONSTANTS.missionLayouts[maxPlayers - 5],
+      maxPlayers: maxPlayers,
+      chosenRoles: chosenRoles,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
   }),
-  joinGame:game(function(game){
-    if(game.players.indexOf(Meteor.user().username) !== -1)
+  joinGame: game(function(game) {
+    if (game.players.indexOf(Meteor.user().username) !== -1)
       return;
-    Games.update(
-      {_id:game._id},
-      {
-        $push:{
-          players:Meteor.user().username
-        },
-        $inc: {currentPlayerCount:1}
+    Games.update({
+      _id: game._id
+    }, {
+      $push: {
+        players: Meteor.user().username
+      },
+      $inc: {
+        currentPlayerCount: 1
       }
-    )
+    })
   }),
-  leaveGame:game(function(game){
-    if(game.players.indexOf(Meteor.user().username) === -1)
+  leaveGame: game(function(game) {
+    if (game.players.indexOf(Meteor.user().username) === -1)
       return;
-    Games.update(
-      { _id: game._id},
-      {
-        $pull: { currentPlayerNames: Meteor.user().username },
-        $inc: { currentPlayerCount: -1 }
+    Games.update({
+      _id: game._id
+    }, {
+      $pull: {
+        currentPlayerNames: Meteor.user().username
+      },
+      $inc: {
+        currentPlayerCount: -1
       }
-    );
+    });
   }),
-  closeGame:game(function(game){
-    if(game.owner === Meteor.userId())
-      Games.remove({_id:game._id});
+  closeGame: game(function(game) {
+    if (game.owner === Meteor.userId())
+      Games.remove({
+        _id: game._id
+      });
   })
 });
